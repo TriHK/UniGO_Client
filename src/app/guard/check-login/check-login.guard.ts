bb5afa7ff589d967085afa34b8fb80d7e41e123a
Nam Thanh Le <lethanhnam2882@gmail.com>
2018-08-19 17:08:55 +0700

    Change authentication mechanism to adapte with back-end

--- src/app/guard/check-login/check-login.guard.ts
+++ src/app/guard/check-login/check-login.guard.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
-import {LoginService} from '../../service/login/login.service';
-import {BaseService} from '../../service/base-service/base.service';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { LoginService } from '../../service/login/login.service';
+import { BaseService } from '../../service/base-service/base.service';
+import { Observable } from "rxjs";
 
 @Injectable()
 export class CheckLoginGuard implements CanActivate {
-  constructor(private loginService: LoginService, private baseService: BaseService,private router: Router) {}
-  canActivate() {
-    const status = this.loginService.checkLogged();
-    if (!status) {
-       this.baseService.showLoginForm();
-       // this.router.navigate(['home']);
+    constructor(private loginService: LoginService, private baseService: BaseService,
+        private router: Router) {
+
     }
-    return status;
-  }
+
+    canActivate() {
+        var tmp = this;
+        var observable = this.loginService.authentication()
+            .map((rs: boolean) => {
+                if (!rs) {
+                    tmp.baseService.showLoginForm();
+                }
+                return rs;
+            });
+        return observable;
+    }
 }
--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,45 +1,39 @@
 export class User {
-  username: string;
-  image: string;
-  name: string;
-  email: string;
-  id: string;
-  providerName: string;
-  role = {
-    id: ''
-  };
-  constructor(data: any) {
-    if (data) {
-     if(data.image){
-       this.image = data.image;
-     }else{
-       this.image = '../../../assets/image/avatar-default.jpg';
-     }
-     if(data.name){
-       this.name = data.name;
-     }else{
-       this.name = data.username;
-     }
-     if(data.userId){
-       this.id = data.userId;
-     }else{
-       this.id = data.id;
-     }
-     if(data.uid){
-       this.id = data.uid;
-     }
-     if(data.provider){
-       this.providerName = data.provider;
-     }
-     this.email = data.email;
-     if(data.role){
-       this.role.id = data.role.id;
-     }else{
-       this.role.id = "1";
-     }
+    username: string;
+    image: string;
+    name: string;
+    email: string;
+    providerName: string;
+    token: string;
+    role: string;
+    constructor(data: any) {
+        if (data) {
+            if (data.image) {
+                this.image = data.image;
+            } else {
+                this.image = '../../../assets/image/avatar-default.jpg';
+            }
+            if (data.name) {
+                this.name = data.name;
+            } else {
+                this.name = data.username;
+            }
+            if (data.username) {
+                this.username = data.username;
+            }
+            if (data.provider) {
+                this.providerName = data.provider;
+            }
+            if (data.token) {
+                this.token = data.token;
+            }
+            this.email = data.email;
+            if (data.role) {
+                this.role = data.role;
+            }
+        }
     }
-  }
-  // public getRole(){
-  //   return this.role;
-  // }
+    // public getRole(){
+    //   return this.role;
+    // }
 }
